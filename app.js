25c446ce74f207099869b9a80fb2fc8b62967fa8
ammy203 <33599273+ammy203@users.noreply.github.com>
2018-02-24 13:49:09 +0500

    Error handling

--- app.js
+++ app.js
@@ -6,14 +6,39 @@ const yarg = require('yargs');
 const _ = require('lodash');
 const notes = require('./src/notes.js')
 
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+  };
+  const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+  };
+  const argv = yarg
+    .command('add', 'Add a new note', {
+      title: titleOptions,
+      body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+      title: titleOptions,
+    })
+    .command('remove', 'Remove a note', {
+      title: titleOptions
+    })
+    .help()
+    .argv;
+
 const command = yarg.argv._[0].toLocaleLowerCase();
 
 if (command == 'add') {
-    notes.addNote(yarg.argv.title,yarg.argv.body);
+    notes.addNote(argv.title,argv.body);
 } else if (command == 'remove') {
-    notes.removeNote(yarg.argv.title);
+    notes.removeNote(argv.title);
 } else if (command == 'read') {
-    notes.readNote(yarg.argv.title);
+    notes.readNote(argv.title);
 } else if (command == 'list') {
     notes.list();
 };
